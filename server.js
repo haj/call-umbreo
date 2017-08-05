ac46cebbb21cad6b62fab7ed2a8bd3ca35f3fda5
Zakaria Boualaid <boulaidzac@gmail.com>
2017-08-05 18:36:26 +0100

    Fix https

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var $ = require('jquery')
 var http = require('http')
+var https = require('https')
 var express = require('express')
-var fs = require('fs');
+var fs = require('fs')
 var app = express()
-var ExpressPeerServer = require('peer').ExpressPeerServer;
+var ExpressPeerServer = require('peer').ExpressPeerServer
 
 const port = process.env.CU_PORT
 const host = process.env.CU_HOSTNAME
@@ -13,14 +14,28 @@ app.set('view engine', 'ejs')
 app.use('/assets', express.static(__dirname + '/public'))
 app.use('/assets', express.static(__dirname + '/lib'))
 
-var httpServer = http.createServer(app)
+var ssl = {
+    key: fs.readFileSync('../callumbreo-key.pem', 'utf8'),
+    cert: fs.readFileSync('../callumbreo-cert.pem', 'utf8')
+}
 
-var server = httpServer.listen(port, host, () => {
-    console.log(`Server Started at http://${host}:${port}`)
+var httpServer = http.createServer(app)
+var httpsServer = https.createServer(ssl, app)
+
+var server = httpServer.listen(9000, '0.0.0.0', () => {
+    console.log(`Server Started at http://0.0.0.0:9000`)
+})
+
+var securedServer = httpsServer.listen(port, host, () => {
+    console.log(`Server Started at https://${host}:${port}`)
 });
 
 app.use('/peerjs', ExpressPeerServer(server, {
     debug: true
+}))
+
+app.use('/peerjs', ExpressPeerServer(securedServer, {
+    debug: false
 }));
 
 app.get('/\:id\?', (req, res) => {
