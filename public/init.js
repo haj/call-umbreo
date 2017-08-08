dc1d65bafa74a0c2d59d558e75c96cb5402f9140
Zakaria Boualaid <boulaidzac@gmail.com>
2017-08-08 04:59:08 +0100

    Implemeting usage of /pathname instead of weird IDs

--- public/init.js
+++ public/init.js
@@ -1,12 +1,16 @@
-const id = window.location.pathname.split('/')[1]
+const peer_id = window.location.pathname.split('/')[1] || null
+const peerServerPayload = {host: location.hostname, port: location.port, path: '/peerjs'}
+const max_users = 8
+var users_count = undefined
 var $join_btn = $('input#join')
 var $endcall_btn = $('input#endcall')
 var $mute_btn = $('input#mute')
 var $camera_btn = $('input#camera')
 var vendorUrl = window.URL || window.webkitURL;
-var peer = new Peer({host: location.hostname, port: location.port, path: '/peerjs'})
 var mediaConnection = null
 var dataConnection = null
+var firstJoiner = true
+var peer = null
 var vendorUrl = window.URL || window.webkitURL;
 var me = document.getElementById('me');
 me.controls = false;
@@ -14,7 +18,7 @@ me.muted = true;
 var partner = document.getElementById('partner');
 partner.controls = false;
 
-(function(){
+$(function(){
 	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 	navigator.getUserMedia({audio: true, video: true}, function(stream){
         window.localStream = stream;
@@ -30,44 +34,5 @@ partner.controls = false;
 	$camera_btn.click(() => {
 		window.localStream.getVideoTracks()[0].enabled = !(window.localStream.getVideoTracks()[0].enabled);
 	})
-
-	$endcall_btn.click(function() {
-		console.log('Terminating...');
-		if(mediaConnection != null)
-			mediaConnection.close();
-		if(dataConnection != null)
-			dataConnection.close();
-	})
-
-	peer.on('open', function(peer_id) {
-		console.log('My peer ID is: ' + peer_id)
-		$('#yourpid').empty().html(peer_id)
-	})
-
-	peer.on('connection', function(data){
-		console.log(data);
-		dataConnection = data
-		console.log('connected!')
-	})
-
-	peer.on('call', function(media) {
-		console.log('Answering a new call...');
-		mediaConnection = media
-
-		media.answer(window.localStream);
-
-		media.on('stream', function(stream) {
-			$("#partner").prop("src", vendorUrl.createObjectURL(stream));
-		});
-
-		media.on('error', (err) => {
-			console.log(err)
-		})
-	});
-
-	peer.on('error', (err) => {
-		console.log(err.type)
-	})
-	
-})();
+});
 
--- public/main.js
+++ public/main.js
@@ -1,28 +1,135 @@
-$join_btn.click(function(){
-    var call_id = $('input#callpid').val()
-    console.log(`Making a Call with ${call_id}`)
+$(function(){
+    // attempt to auto-join with the id in pathname
+    ConnectToExistingPeer
+        .then((dataConnection) => {
+            console.log('connection ready! from promise CONSUMPTION');
+            if(dataConnection.open){
+                console.log('calling peer')
 
-    dataConnection = peer.connect(call_id)
+                call_peer(peer_id)
+                    .then((call) => {
+                        call.on('stream', function(stream) {
+                            $("#partner").prop("src", vendorUrl.createObjectURL(stream))
+                        })
+
+                        call.on('error', (err) => {
+                            console.log(err)
+                        })
+                    })
+                    .catch((call) => {
+                        console.log('got nothing')
+                        // console.log(call)
+                    })
+            } else {
+                // console.log('init peer');
+                // init_peer()
+            }
+        })
+})
+
+var ConnectToExistingPeer = new Promise((resolve, reject) => {
+    // var call_id = $('input#callpid').val()
+    console.log('Attempt to connect to an existing peer...')    
+    console.log(`Connecting to peer : ${peer_id}`)
+
+    init_peer()
+    // peer = new Peer({host: location.hostname, port: location.port, path: '/peerjs'})
+    dataConnection = peer.connect(peer_id)
 
     dataConnection.on('open', () => {
-        console.log('connection ready!');
-
-        mediaConnection = peer.call(call_id, window.localStream)
-
-        mediaConnection.on('stream', function(stream) {
-            $("#partner").prop("src", vendorUrl.createObjectURL(stream))
-        })
-
-        mediaConnection.on('error', (err) => {
-            console.log(err);
-        })
+        console.log('dataConnection.open');
+        resolve(dataConnection)
     })
 
-    dataConnection.on('data', (data) => {
-        console.log('data received!');
-    })
-    
     dataConnection.on('error', (err) => {
         console.log('error during connection!');
     })
+
+    dataConnection.on('close', () => {
+        if(Object.keys(peer.connections).length <= 1){
+            peer = null
+            init_peer(peer_id)
+        } else {
+
+        }
+        console.log('connection closed from the other side!');
+    })
+
+    // reject()
+    console.log(dataConnection)
+})
+
+function init_peer(_id=null){
+    console.log(`init peer(${_id}) ->`)
+
+    if(!_id) {
+        peer = new Peer(peerServerPayload)
+    } else {
+        peer = new Peer(_id, peerServerPayload)
+    }
+
+    peer.on('open', function(peer_id) {
+        console.log(`New peer initiated with ID : ${peer_id}`)
+        $('#yourpid').empty().html(peer_id)
+    })
+
+    peer.on('connect', function(peer_id) {
+        console.log('connected! connect event');
+    })
+
+    peer.on('connection', function(data){
+        console.log(data);
+        dataConnection = data
+        console.log('connected!')
+    })
+
+    peer.on('call', function(call) {
+        console.log('Answering a new call...');
+        mediaConnection = call
+
+        call.answer(window.localStream);
+
+        call.on('stream', function(stream) {
+            $("#partner").prop("src", vendorUrl.createObjectURL(stream));
+        });
+
+        call.on('error', (err) => {
+            console.log(err)
+        })
+    });
+
+    peer.on('error', (err) => {
+        switch (err.type) {
+            case 'peer-unavailable':
+                console.log('You are the first joiner!');
+                peer = null
+                init_peer(peer_id)
+                break;
+            default:
+                console.log(err.type);
+                break;
+        }
+    })
+}
+
+function call_peer(peer_id) {
+    return new Promise((resolve, reject) => {
+        (function waitForAnswer() {
+            console.log('still no mediaConnection');
+            mediaConnection = peer.call(peer_id, window.localStream)
+            if(mediaConnection) { 
+                return resolve(mediaConnection)
+            } else {
+                setTimeout(waitForAnswer, 500)
+            }
+        })()
+    })
+}
+
+$endcall_btn.click(function() {
+    console.log('Terminating...');
+    if(mediaConnection != null)
+        mediaConnection.close();
+    if(dataConnection != null)
+        dataConnection.close();
 })
